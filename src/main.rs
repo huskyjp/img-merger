0f91e2dc395f7386e268bd2e67387c82fc5da4ca
huskyjp <taiseaocean@gmail.com>
2021-12-13 00:04:02 +0900

    impl image fn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 mod args;
 use args::Args;
+use image::{ io::Reader, DynamicImage, ImageFormat};
+use std::{ io::BufReader, fs::File };
 
 fn main() {
     let args = Args::new();
     println!("Hello, world!");
 }
 
+fn get_tuple_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+
+   return (image, image_format)
+}
